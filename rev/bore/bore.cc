76b6ae824b440eaaa69ff464430bdb66663ec5f9
alphanumeric <99887023+alphanumuser@users.noreply.github.com>
2025-05-13 07:39:16 +0800

    Update bore.cc

--- rev/bore/bore.cc
+++ rev/bore/bore.cc
@@ -4,9 +4,6 @@ using namespace std;
 string flag = "deadbeef{REDACTED}";
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
   cout << ">>> ";
   int inp;
   cin >> inp;
